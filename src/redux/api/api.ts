cc469b72acfde7132ca084910df51c6ff7de750a
ebrahimkhalil074 <mdebrahimkhalil074@gmail.com>
2024-09-30 18:36:26 +0600

    vercel url

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { TQueryParams, TResponseRedux } from "../../types/globalTypes";
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://fitness-shop-becend.vercel.app/api" }),
   tagTypes: ["products", "verify"],
   endpoints: (builder) => ({
 
